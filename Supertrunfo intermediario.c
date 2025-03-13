71f679027d3cec0397b80b23bc147187f7e47c6c
Bryan-Reis <brbryan06@gmail.com>
2025-03-13 14:55:14 -0300

    Update Supertrunfo intermediario.c

--- Supertrunfo intermediario.c
+++ Supertrunfo intermediario.c
@@ -47,7 +47,7 @@ int main(){
     float PIBCAP = (float) PIB / população;
     printf("O PIB por capital é: %.2f", PIBCAP);
 
-     printf("Carta 2 \nNome: %s\nEstado: %s\n", &nome, &estado);
+    printf("Carta 1 \nNome: %s\nEstado: %s\n", &nome, &estado);
     printf("Codigo %c%s\n", estado, codigo);
     printf("População: %d\nPib: %.2f\nArea: %.2f\nPontos turisticos: %d\nDensidade: %.2f\nPIB por capital: %.2f\n",população, PIB, area, Npontosturisticos, densidade, PIBCAP);
 
